c68b02d8378a3e06ef8c14f7492ccf63e04bb34f
kimsanghoon1 <sanghoon01@uengine.org>
2020-01-16 11:12:53 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8876e624851284a0833e0549ab03c618.js"
+  "/precache-manifest.986563ed3494faee701f6aef673e94d3.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "service-ui"});
