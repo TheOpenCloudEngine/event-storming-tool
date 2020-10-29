f349d8d56878dac5c1ef426776abdd30a2de1c9f
lee-soo-heon <sooheon45@uengine.org>
2020-10-29 18:34:17 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.88409726ab917b4ebe5a9fd32f58ec5c.js"
+  "/precache-manifest.81604f4b53d0c7b0e30b6e4d7219b150.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "service-ui"});
